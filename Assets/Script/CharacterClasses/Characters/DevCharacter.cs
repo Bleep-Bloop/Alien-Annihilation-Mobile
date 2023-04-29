cb01faff56c83a47e03ad2fc3995861a795746ad
Taisen Colcher <TaisenColcher@Gmail.com>
2023-04-29 01:02:13 -0400

    Create DevCharacter prefab
    Created a character prefab for testing.
    Added prefab to scene.
    Created folder's to store parent/child Character classes.

---
+++ Assets/Script/CharacterClasses/Characters/DevCharacter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class DevCharacter : BaseCharacter
+{
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+}
