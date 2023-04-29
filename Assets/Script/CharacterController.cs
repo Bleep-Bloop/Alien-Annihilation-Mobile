2bd394ca830329ecdd2862d542a66fae66d34d23
Taisen Colcher <TaisenColcher@Gmail.com>
2023-04-29 00:57:56 -0400

    Implement movement in CharacterController.
    Implemented movement inside the player's character controller class.
    Uses movement speed property from the parent character class.

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BaseCharacter))]
 public class CharacterController : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private FloatingJoystick variableJoystick; // ToDo: Maybe grab from scene
+    private Rigidbody2D rb2D; 
+    private BaseCharacter parentCharacter;
+
+    [Header("Runtime")]
+    private Vector3 MovementVector = new();
+
+    private void Awake()
+    {
+        parentCharacter = GetComponent<BaseCharacter>();
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        Move();
     }
+
+    void Move()
+    {
+        MovementVector.x = variableJoystick.Horizontal * parentCharacter.GetMovementSpeed();
+        MovementVector.y = variableJoystick.Vertical * parentCharacter.GetMovementSpeed();
+        rb2D.velocity = MovementVector;
+    }
+
 }
